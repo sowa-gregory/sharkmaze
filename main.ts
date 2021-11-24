d4fbaf7af52e3be184747f38461c4993d5c91af9
sowisz <38260370+sowa-gregory@users.noreply.github.com>
2021-11-24 17:50:02 +0100

    obracanie rekina

--- main.ts
+++ main.ts
@@ -2,9 +2,49 @@ scene.onOverlapTile(SpriteKind.Player, sprites.swamp.swampTile3, function (sprit
     Shark.destroy(effects.fire, 500)
     Shark.setVelocity(0, 0)
 })
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    Shark.setImage(img`
+        .............ccfff..............
+        ............cddbbf..............
+        ...........cddbbf...............
+        ..........fccbbcf............ccc
+        ....ffffffccccccff.........ccbbc
+        ..ffbbbbbbbbbbbbbcfff.....cdbbc.
+        ffbbbbbbbbbcbcbbbbcccff..cddbbf.
+        fbcbbbbbffbbcbcbbbcccccfffdbbf..
+        fbbb1111ff1bcbcbbbcccccccbbbcf..
+        .fb11111111bbbbbbcccccccccbccf..
+        ..fccc33cc11bbbbccccccccfffbbcf.
+        ...fc131c111bbbcccccbdbc...fbbf.
+        ....f33c111cbbbfdddddcc.....fbbf
+        .....ff1111fbdbbfddcc........fff
+        .......cccccfbdbbfc.............
+        .............fffff..............
+        `)
+})
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleBlueCrystal, function (sprite, location) {
     tiles.setTileAt(location, sprites.dungeon.collectibleInsignia)
     info.changeScoreBy(1)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    Shark.setImage(img`
+        ..............fffcc.............
+        ..............fbbddc............
+        ...............fbbddc...........
+        ccc............fcbbccf..........
+        cbbcc.........ffccccccffffff....
+        .cbbdc.....fffcbbbbbbbbbbbbbff..
+        .fbbddc..ffcccbbbbcbcbbbbbbbbbff
+        ..fbbdfffcccccbbbcbcbbffbbbbbcbf
+        ..fcbbbcccccccbbbcbcb1ff1111bbbf
+        ..fccbcccccccccbbbbbb11111111bf.
+        .fcbbfffccccccccbbbb11cc33cccf..
+        .fbbf...cbdbcccccbbb111c131cf...
+        fbbf.....ccdddddfbbbc111c33f....
+        fff........ccddfbbdbf1111ff.....
+        .............cfbbdbfccccc.......
+        ..............fffff.............
+        `)
 })
 sprites.onDestroyed(SpriteKind.Player, function (sprite) {
     game.over(false)

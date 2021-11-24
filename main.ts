ac63ebe544032230e8f7efe3f40acdc5e598226a
sowisz <38260370+sowa-gregory@users.noreply.github.com>
2021-11-24 15:20:16 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+tiles.setTilemap(tilemap`Level1 m M`)
+let Shark = sprites.create(img`
+    .............ccfff..............
+    ............cddbbf..............
+    ...........cddbbf...............
+    ..........fccbbcf............ccc
+    ....ffffffccccccff.........ccbbc
+    ..ffbbbbbbbbbbbbbcfff.....cdbbc.
+    ffbbbbbbbbbcbcbbbbcccff..cddbbf.
+    fbcbbbbbffbbcbcbbbcccccfffdbbf..
+    fbbb1111ff1bcbcbbbcccccccbbbcf..
+    .fb11111111bbbbbbcccccccccbccf..
+    ..fccc33cc11bbbbccccccccfffbbcf.
+    ...fc131c111bbbcccccbdbc...fbbf.
+    ....f33c111cbbbfdddddcc.....fbbf
+    .....ff1111fbdbbfddcc........fff
+    .......cccccfbdbbfc.............
+    .............fffff..............
+    `, SpriteKind.Player)
+controller.moveSprite(Shark)
+scene.cameraFollowSprite(Shark)
+effects.bubbles.startScreenEffect()

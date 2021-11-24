8157b56f860500fb774a022beb0a55cf8196f5f5
sowisz <38260370+sowa-gregory@users.noreply.github.com>
2021-11-24 18:26:23 +0100

    update

--- main.ts
+++ main.ts
@@ -24,6 +24,7 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleBlueCrystal, function (sprite, location) {
     tiles.setTileAt(location, sprites.dungeon.collectibleInsignia)
+    music.sonar.play()
     info.changeScoreBy(1)
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -69,7 +70,29 @@ Shark = sprites.create(img`
     .......cccccfbdbbfc.............
     .............fffff..............
     `, SpriteKind.Player)
+Shark.setPosition(50, 48)
+let enemy1 = sprites.create(img`
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . . . . b c . . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 d 1 f 5 5 d f . . 
+    . . . . b 5 5 1 f f 5 d 4 c . . 
+    . . . . b 5 5 d f b d d 4 4 . . 
+    b d d d b b d 5 5 5 4 4 4 4 4 b 
+    b b d 5 5 5 b 5 5 4 4 4 4 4 b . 
+    b d c 5 5 5 5 d 5 5 5 5 5 b . . 
+    c d d c d 5 5 b 5 5 5 5 5 5 b . 
+    c b d d c c b 5 5 5 5 5 5 5 b . 
+    . c d d d d d d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    `, SpriteKind.Enemy)
+enemy1.setVelocity(0, 0)
+enemy1.setBounceOnWall(true)
+tiles.placeOnTile(enemy1, tiles.getTileLocation(25, 11))
 controller.moveSprite(Shark, 100, 100)
 scene.cameraFollowSprite(Shark)
 effects.bubbles.startScreenEffect()
-info.startCountdown(200)
+info.startCountdown(30)
